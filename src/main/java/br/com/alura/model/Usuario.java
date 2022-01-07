e1536b92f4db757acfa03a1121a56c7677d3a792
Caroline Araujo <ca.araujosouza@gmail.com>
2022-01-07 16:05:26 -0300

    add entity and tables

---
+++ src/main/java/br/com/alura/model/Usuario.java
@@ -0,0 +1,39 @@
+package br.com.alura.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+@Entity
+public class Usuario extends PanacheEntityBase {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	
+	private String cpf;
+	
+	private String username;
+	
+	private String password;
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	
+
+}
